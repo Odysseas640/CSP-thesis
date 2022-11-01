4c8a9f019a309d89333065be8384c62f9adedc7d
Odysseas Stefas <68035017+Odysseas640@users.noreply.github.com>
2022-11-02 00:01:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[]) {
 	strcpy(pairings_file, "./Pairings.txt");
 	// Read all flight plans and put them in a vector
 	std::vector<flightPlan*> *flight_plans_vector = new std::vector<flightPlan*>;
-	read_file_into_vector(flight_plans_vector, pairings_file, 200);
+	read_file_into_vector(flight_plans_vector, pairings_file, -1);
 
 	// Store flight plan start/end datetimes as minutes from START_DATE (2011/11/1)
 	set_minutes_from_global_start_date(flight_plans_vector, 1, 11, 2011);
@@ -24,54 +24,51 @@ int main(int argc, char const *argv[]) {
 	// that says who's the pilot.
 	// There are 3304 flight plans. Try 3304 pilots and decrease.
 	// int pilots = flight_plans_vector->size();
-	int pilots = 200;
+	int pilots = 26;
 	naxos::NsProblemManager pm;
-	naxos::NsIntVarArray FP_var_array;
-	naxos::NsIntVarArray FP_start_time_array, FP_end_time_array;
+	naxos::NsIntVarArray FP;
+	naxos::NsIntVarArray FP_start, FP_end;
 	for (int i = 0; i < (int) flight_plans_vector->size(); ++i) {
-		FP_var_array.push_back(naxos::NsIntVar(pm, 1, pilots));
-		FP_start_time_array.push_back(naxos::NsIntVar(pm, 1, 123456));
-		FP_start_time_array[i].set(flight_plans_vector->at(i)->start);
-		FP_end_time_array.push_back(naxos::NsIntVar(pm, 1, 123456));
-		FP_end_time_array[i].set(flight_plans_vector->at(i)->end);
+		FP.push_back(naxos::NsIntVar(pm, 1, pilots));
+		FP_start.push_back(naxos::NsIntVar(pm, 1, 180000)); // 4 months = 172800 minutes + safe margin
+		FP_start[i].set(flight_plans_vector->at(i)->start);
+		FP_end.push_back(naxos::NsIntVar(pm, 1, 180000));
+		FP_end[i].set(flight_plans_vector->at(i)->end);
 	}
-	pm.addGoal(new naxos::NsgLabeling(FP_var_array));
-	// If 2 FPs have the same pilot, they cannot overlap. HOW TO DO THIS MORE EFFICIENTLY
-	for (int i = 0; i < (int) flight_plans_vector->size(); ++i) {
+	pm.addGoal(new naxos::NsgLabeling(FP));
+
+	// If 2 FPs have the same pilot, they cannot overlap.
+	for (int i = 0; i < (int) flight_plans_vector->size(); ++i) { // With only this constraint, 100 FPs need 11 pilots.
 		for (int j = 0; j < (int) flight_plans_vector->size(); ++j) {
 			if (i == j)
 				continue;
-			pm.add(naxos::NsIfThen((/* i starts before j ends */ FP_start_time_array[i] <= FP_end_time_array[j] && /* i starts after j starts*/ FP_start_time_array[i] >= FP_start_time_array[j])
-			|| (/* i ends after j starts */ FP_end_time_array[i] >= FP_start_time_array[j] && /* i ends before j ends */ FP_end_time_array[i] <= FP_end_time_array[j]),
-			/*CANT HAVE SAME PILOT*/ FP_var_array[i]!=FP_var_array[j]));
+			else if (/* i starts before j ends */ FP_start[i].value() <= FP_end[j].value() && /* i starts after j starts*/ FP_start[i].value() >= FP_start[j].value())
+				pm.add(FP[i]!=FP[j]);
+			else if (/* i ends after j starts */ FP_end[i].value() >= FP_start[j].value() && /* i ends before j ends */ FP_end[i].value() <= FP_end[j].value())
+				pm.add(FP[i]!=FP[j]);
 		}
 	}
 
 	// If 2 FPs have the same pilot, they must be 660+ minutes apart.
-	naxos::NsIntVar minutes660(pm, 660, 660);
-	minutes660.set(660);
-	for (int i = 0; i < (int) flight_plans_vector->size(); ++i) {
+	for (int i = 0; i < (int) flight_plans_vector->size(); ++i) { // With this constraint too, 100 FPs need 24 pilots.
 		for (int j = 0; j < (int) flight_plans_vector->size(); ++j) {
 			if (i == j)
 				continue;
-			pm.add(naxos::NsIfThen(/* i ends within 11 hours of j starting */ FP_start_time_array[j] - FP_end_time_array[i] < 660
-			|| /* i starts within 11 hours of j ending */ FP_end_time_array[i] - FP_end_time_array[j] < 660,
-			/*CANT HAVE SAME PILOT*/ FP_var_array[i]!=FP_var_array[j]));
+			else if (FP_start[i].value() >= FP_end[j].value() && FP_start[i].value() - FP_end[j].value() <= 660) // If i starts after j ends, and within 11 hours
+				pm.add(FP[i]!=FP[j]);
+			else if (FP_start[j].value() >= FP_end[i].value() && FP_start[j].value() - FP_end[i].value() <= 660) // If j starts after i ends, and within 11 hours
+				pm.add(FP[i]!=FP[j]);
 		}
 	}
 
-	// int solution_n = 0;
 	if (pm.nextSolution() != false) {
 		printf("\n  --  SOLUTION  --\n");
-		// solution_n++;
 		for (int i = 0; i < (int) flight_plans_vector->size(); ++i) {
-			// if (solution_n == 1)
-				printf("Flight plan %d -> pilot %ld\n", i+1, FP_var_array[i].value());
+				printf("Flight plan %d -> pilot %ld\n", i+1, FP[i].value());
 		}
 	}
 	else
 		printf("\n  --  NO SOLUTION  --\n\n");
-	// printf("Iterations (legal solutions): %d\n", solution_n);
 	/////////////////////////////////////////////////////////////////
 
 	for (int i = 0; i < (int) flight_plans_vector->size(); ++i)
