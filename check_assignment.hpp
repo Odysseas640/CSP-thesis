d8fcfbd882a9811ce788c8cfdf5e8704ccbd48a0
Odysseas Stefas <odysseas640@gmail.com>
2023-02-12 06:02:30 -0800

    1

--- check_assignment.hpp
+++ check_assignment.hpp
@@ -7,9 +7,6 @@
 #include "flight_plan.hpp"
 
 int check_assignment(const std::vector<flightPlan*>*, const DateTime&, const DateTime&);
-void copy_to_final_assignments(std::vector< std::vector<int> * > *&, const std::vector<flightPlan*>*, const naxos::NsIntVarArray& PilotAssignments, int);
-void print_final_assignments(const std::vector< std::vector<int> * > *);
-void print_final_assignments_to_file(const std::vector< std::vector<int> * > *);
 int check_assignments_11h_gap(const std::vector<std::vector<flightPlan*>*>*);
 int check_assignments_rolling_weeks(const std::vector<std::vector<flightPlan*>*>*);
 
